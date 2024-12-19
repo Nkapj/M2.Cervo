0047eaea59e172064b461b1d8b5a052518128473
Nkapj <jessica.nzi@outlook.com>
2024-12-19 22:55:29 +0100

    first app react

--- src/ProductTech.jsx
+++ src/ProductTech.jsx
@@ -1,10 +1,24 @@
 
+import { useEffect } from 'react';
 import Product from './data/productTech.json';
 
 
 function ProducTech () {
     const [productList, setProductList] = useState(products);
 
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                const response = await fetch('http://localhost:4000/');
+                const data = await response.json();
+            } catch (error) {
+            console.log ("error")
+            }
+        }
+        
+    
+    },[]);
 
 
 
